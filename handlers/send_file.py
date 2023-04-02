1152e6d09d6d5ddbef367d116d9f8675834570f0
Ruban Durai <77948100+RubanDurai@users.noreply.github.com>
2023-04-02 17:57:04 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -28,6 +28,10 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
             return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                               message_ids=file_id)
 
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
+        return media_forward(bot, user_id, file_id)
+
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
     await reply_forward(message=sent_message, file_id=file_id)
