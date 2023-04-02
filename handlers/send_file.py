bf368b9e74218deb263d86a3af6da2e6d7048e9a
Ruban Durai <77948100+RubanDurai@users.noreply.github.com>
2023-04-02 15:10:56 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -20,17 +20,16 @@ async def reply_forward(message: Message, file_id: int):
         await reply_forward(message, file_id)
 
 
-async def media_forward(bot: Client, user_id: int, file_id: int):
+def media_forward(bot: Client, user_id: int, file_id: int):
     try:
         if Config.FORWARD_AS_COPY is True:
-            return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
-                                          message_id=file_id)
+             return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+                                             message_id=file_id)
         elif Config.FORWARD_AS_COPY is False:
             return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                               message_ids=file_id)
-    except FloodWait as e:
-        await asyncio.sleep(e.value)
-        return media_forward(bot, user_id, file_id)
+        except Exception as e:
+            print("Error forwarding message: {}".format(e))
 
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
