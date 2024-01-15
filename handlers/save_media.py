40c7305c0ef6791c4fd6c6b040bc6b710a01b8cf
Ruban Durai <77948100+RubanDurai@users.noreply.github.com>
2024-01-15 17:54:56 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -55,7 +55,7 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
                 InlineKeyboardButton("Delete Batch", callback_data="closeMessage")
             ]])
         )
-        share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=ThammuTV_{str_to_b64(str(SaveMessage.id))}"
+        share_link = f"https://fileshare.tnrockers.workers.dev/?start=ThammuTV_{str_to_b64(str(SaveMessage.id))}"
         short_url = await short_link(share_link)
         await editable.edit(
             f"**Batch Files Stored in my Database!**\n\nHere is the Permanent Link of your files: {share_link} \n\n"
@@ -94,7 +94,7 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
         await forwarded_msg.reply_text(
             f"#PRIVATE_FILE:\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) Got File Link!",
             disable_web_page_preview=True)
-        share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=ThammuTV_{str_to_b64(file_er_id)}"
+        share_link = f"https://fileshare.tnrockers.workers.dev/?start=ThammuTV_{str_to_b64(file_er_id)}"
         short_url = await short_link(share_link)
         # get media type
         media_type = message.document or message.video or message.audio or message.photo
